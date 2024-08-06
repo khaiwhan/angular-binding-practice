810996596382a89a16aa6319f2a68f6f2875dc0c
khaiwhan <41568906+khaiwhan@users.noreply.github.com>
2024-08-06 13:04:13 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-binding-practice
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/khaiwhan/angular-binding-practice)
---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  v1: string = '';
+  v2: number = 0;
+  v3: string;
+}
